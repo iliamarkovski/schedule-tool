a5b295ac940c779940fd22beaa595accc4c5531a
iliamarkovski <markovskiilia@gmail.com>
2024-05-20 18:41:25 +0300

    Add atom/Icon

---
+++ src/components/atoms/Icon/Icon.tsx
@@ -0,0 +1,13 @@
+import IcomoonReact from 'icomoon-react';
+import iconSet from './selection.json';
+
+type Props = {
+  name: string;
+  className?: string;
+};
+
+const Icon = ({ name, className = '' }: Props) => {
+  return <IcomoonReact className={className} iconSet={iconSet} icon={name} />;
+};
+
+export { Icon };
