b048f4dd24af6f99c44c2104402adfd88de1ee4b
iliamarkovski <markovskiilia@gmail.com>
2024-05-21 17:41:24 +0300

    Refactor ScheduleSliderButtons

--- src/components/molecules/ScheduleSliderButtons/ScheduleSliderButtons.tsx
+++ src/components/molecules/ScheduleSliderButtons/ScheduleSliderButtons.tsx
@@ -1,16 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { ArrowButton } from '../../atoms/ArrowButton/ArrowButton';
 import styles from './ScheduleSliderButtons.module.scss';
+import { AppDispatch, RootState } from '../../../store';
+import { updateScrollStep } from '../../../store/slices/schedule';
 
 export const ScheduleSliderButtons = () => {
+  const times = useSelector((state: RootState) => state.schedule.times);
+  const scrollStep = useSelector(
+    (state: RootState) => state.schedule.scrollStep
+  );
+  const dispatch: AppDispatch = useDispatch();
+
+  const itemsToScroll = times.length - 7;
+
+  const handleNextClick = () => {
+    dispatch(updateScrollStep(scrollStep + 1));
+  };
+
+  const handlePrevClick = () => {
+    dispatch(updateScrollStep(scrollStep - 1));
+  };
+
   return (
     <div className={styles.container}>
       <ArrowButton
-        onClick={() => console.log(1)}
+        onClick={handlePrevClick}
         iconName='arrow-left'
-        disabled
+        disabled={scrollStep === 0}
       />
 
-      <ArrowButton onClick={() => console.log(1)} iconName='arrow-right' />
+      <ArrowButton
+        onClick={handleNextClick}
+        iconName='arrow-right'
+        disabled={scrollStep === itemsToScroll}
+      />
     </div>
   );
 };
